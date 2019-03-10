46bf6538e62ccb97a28254f848107a66e917b9cc
David MacCallum <davidjohnmac@gmail.com>
2019-03-10 12:49:27 +0200

    eslint . --fix

--- README.md
+++ README.md
@@ -23,3 +23,14 @@ I want the little red squiggles in my editor to help me find my mistakes. So nex
 yarn add -D eslint babel-eslint
 yarn add -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-import
 ```
+
+(Commit)[https://github.com/davidjohnmaccallum/basic-react-native-skills/commit/5ee719aca6c9af1f19d47eeb742025311fb26a64]
+
+Now I have my little red squiggles. Problem is, my project is covered with them and I have not touched a single line of code yet :(
+
+```
+npx eslint . --fix
+```
+
+
+
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -75,16 +75,17 @@ export default class HomeScreen extends React.Component {
       return (
         <Text style={styles.developmentModeText}>
           Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
+          tools.
+          {' '}
+          {learnMoreButton}
         </Text>
       );
     }
+    return (
+      <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+      </Text>
+    );
   }
 
   _handleLearnMorePress = () => {
